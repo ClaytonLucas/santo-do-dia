44b67ae88a5822683df589c4ad4f773b4586f8e0
ClaytonLucas <clayton.lucas07@gmail.com>
2025-02-13 01:49:34 -0300

    Added swagger and changed main to app

--- app.py
+++ app.py
@@ -1,45 +1,60 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
-
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)  # Inicializa o Swagger
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
+
 @app.route("/")
 def home():
+    """
+    Obtém o santo do dia atual.
+    ---
+    responses:
+      200:
+        description: Retorna o santo do dia com nome, imagem, história, reflexão e oração.
+        schema:
+          type: object
+          properties:
+            results:
+              type: array
+              items:
+                type: object
+                properties:
+                  nome:
+                    type: string
+                  imagem:
+                    type: string
+                  historia:
+                    type: string
+                  reflexao:
+                    type: string
+                  oracao:
+                    type: string
+    """
     page = requests.get('https://www.a12.com/reze-no-santuario/santo-do-dia', headers=headers)
-
     soup = BeautifulSoup(page.text, 'html.parser')
     
     saint_list = soup.find("div", class_="saints-list")
     santo = []
+    
     if saint_list:
-       
-        
         for a in saint_list.find_all('a', href=True):
-            
             page = requests.get(a['href'], headers=headers)
             soup = BeautifulSoup(page.text, 'html.parser')
 
-            
             saint_name = soup.find('div', class_='feature__name').text
             saint_infos = soup.find('div', class_='wg-text').find_all('p')
-            info = ""
-            reflexao = ""
-            oracao = ""
+            info = "".join(p.text + "\n\n" for p in saint_infos[:-4])
+            reflexao = "".join(p.text + "\n\n" for p in saint_infos[-3])
+            oracao = "".join(p.text + "\n\n" for p in saint_infos[-1])
             imagem = "https://www.a12.com" + soup.find("div", class_="feature").find(class_="feature__portrait")["src"]
 
-            for saint_info in saint_infos[:-4]:
-                info = info + saint_info.text + "\n\n"
-            
-            for saint_info in saint_infos[-3]:
-                reflexao = reflexao + saint_info.text + "\n\n"
-            
-            for saint_info in saint_infos[-1]:
-                oracao = oracao + saint_info.text + "\n\n"
-            
             santo.append({
                 'nome': saint_name,
                 'imagem': imagem,
@@ -47,25 +62,13 @@ def home():
                 'reflexao': reflexao,
                 'oracao': oracao
             })
-
-        
-
     else:
         saint_name = soup.find('div', class_='feature__name').text
         saint_infos = soup.find('div', class_='wg-text').find_all('p')
-        info = ""
-        reflexao = ""
-        oracao = ""
+        info = "".join(p.text + "\n\n" for p in saint_infos[:-4])
+        reflexao = "".join(p.text + "\n\n" for p in saint_infos[-3])
+        oracao = "".join(p.text + "\n\n" for p in saint_infos[-1])
         imagem = "https://www.a12.com" + soup.find("div", class_="feature").find(class_="feature__portrait")["src"]
-
-        for saint_info in saint_infos[:-4]:
-            info = info + saint_info.text + "\n\n"
-
-        for saint_info in saint_infos[-3]:
-            reflexao = reflexao + saint_info.text + "\n\n"
-
-        for saint_info in saint_infos[-1]:
-            oracao = oracao + saint_info.text + "\n\n"
 
         santo.append({
             'nome': saint_name,
@@ -75,41 +78,65 @@ def home():
             'oracao': oracao
         })
     
-    return jsonify(results = santo)
+    return jsonify(results=santo)
 
 @app.route("/dia=<int:dia>&mes=<int:mes>")
 def date(dia, mes):
-    page = requests.get(f'https://www.a12.com/reze-no-santuario/santo-do-dia?day={dia}&month={mes}', headers=headers)
+    """
+    Obtém o santo celebrado no dia e mês escolhidos pelo usuário.
+    ---
+    parameters:
+      - name: dia
+        in: path
+        type: integer
+        required: true
+        description: Dia do mês desejado.
+      - name: mes
+        in: path
+        type: integer
+        required: true
+        description: Mês desejado.
 
+    responses:
+      200:
+        description: Retorna o santo do dia e mês informados.
+        schema:
+          type: object
+          properties:
+            results:
+              type: array
+              items:
+                type: object
+                properties:
+                  nome:
+                    type: string
+                  imagem:
+                    type: string
+                  historia:
+                    type: string
+                  reflexao:
+                    type: string
+                  oracao:
+                    type: string
+    """
+    page = requests.get(f'https://www.a12.com/reze-no-santuario/santo-do-dia?day={dia}&month={mes}', headers=headers)
     soup = BeautifulSoup(page.text, 'html.parser')
     
     saint_list = soup.find("div", class_="saints-list")
     santo = []
+
     if saint_list:
-        
-        
         for a in saint_list.find_all('a', href=True):
-            
             page = requests.get(a['href'], headers=headers)
             soup = BeautifulSoup(page.text, 'html.parser')
 
-            
             saint_name = soup.find('div', class_='feature__name').text
             saint_infos = soup.find('div', class_='wg-text').find_all('p')
-            info = ""
-            reflexao = ""
-            oracao = ""
+            info = "".join(p.text + "\n\n" for p in saint_infos[:-4])
+            reflexao = "".join(p.text + "\n\n" for p in saint_infos[-3])
+            oracao = "".join(p.text + "\n\n" for p in saint_infos[-1])
             imagem = "https://www.a12.com" + soup.find("div", class_="feature").find(class_="feature__portrait")["src"]
 
-            for saint_info in saint_infos[:-4]:
-                info = info + saint_info.text + "\n\n"
-            
-            for saint_info in saint_infos[-3]:
-                reflexao = reflexao + saint_info.text + "\n\n"
-            
-            for saint_info in saint_infos[-1]:
-                oracao = oracao + saint_info.text + "\n\n"
-            
             santo.append({
                 'nome': saint_name,
                 'imagem': imagem,
@@ -117,25 +144,13 @@ def date(dia, mes):
                 'reflexao': reflexao,
                 'oracao': oracao
             })
-
-        
-
     else:
         saint_name = soup.find('div', class_='feature__name').text
         saint_infos = soup.find('div', class_='wg-text').find_all('p')
-        info = ""
-        reflexao = ""
-        oracao = ""
+        info = "".join(p.text + "\n\n" for p in saint_infos[:-4])
+        reflexao = "".join(p.text + "\n\n" for p in saint_infos[-3])
+        oracao = "".join(p.text + "\n\n" for p in saint_infos[-1])
         imagem = "https://www.a12.com" + soup.find("div", class_="feature").find(class_="feature__portrait")["src"]
-
-        for saint_info in saint_infos[:-4]:
-            info = info + saint_info.text + "\n\n"
-
-        for saint_info in saint_infos[-3]:
-            reflexao = reflexao + saint_info.text + "\n\n"
-
-        for saint_info in saint_infos[-1]:
-            oracao = oracao + saint_info.text + "\n\n"
 
         santo.append({
             'nome': saint_name,
@@ -144,10 +159,8 @@ def date(dia, mes):
             'reflexao': reflexao,
             'oracao': oracao
         })
-    return jsonify(results = santo)
+    
+    return jsonify(results=santo)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
